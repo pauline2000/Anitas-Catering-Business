b6ba24793350abca4db36d92530d1570b7f30395
Sherry <ndungupauline0@gmail.comgit config --global user .name 2000sherrygit config --user .email ndungupauline0@gmail.com>
2018-03-23 12:24:40 +0300

    add links to bootstrap

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  $("p").click(function() {
+    $("img").show();
+  });
+});
+$(document).ready(function() {
+  $("p").click(function() {
+    $("#initially-showing").fadeOut();
+    $("#initially-hidden").fadeIn();
+  });
+});
+$(document).ready(function() {
+  $("p").click(function() {
+    $("#initially-showing").fadeOut();
+    $("#initially-hidden").fadeIn();
+  });
+});
+$(document).ready(function() {
+  $("p").click(function() {
+    $("#initially-showing").fadeOut();
+    $("#initially-hidden").fadeIn();
+  });
+});
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#initially-showing").fadeOut();
+    $("#initially-hidden").fadeOut();
+  });
+});
